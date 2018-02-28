70d82e372236ad9a82351fe76dff4bf7cff32a41
11CS116 <tony_achumi@yahoo.com>
2018-02-28 19:22:42 +0530

    Update OnNotificationOpenReceiver.java

--- src/android/OnNotificationOpenReceiver.java
+++ src/android/OnNotificationOpenReceiver.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 
 public class OnNotificationOpenReceiver extends BroadcastReceiver {
 
@@ -13,7 +14,15 @@ public class OnNotificationOpenReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         PackageManager pm = context.getPackageManager();
         Intent launchIntent = pm.getLaunchIntentForPackage(context.getPackageName());
-
+        Log.d("Notification", "Notification onReceive context.getPackageName(): " + context.getPackageName());
+        Log.d("Notification", "Notification onReceive launchIntent: " + launchIntent);
+        String action=intent.getStringExtra("action");
+        if(action.equals("ALLOW")){
+            Log.d("Notification", "Notification onReceive ALLOW: ");
+        }
+        else if(action.equals("DENY")){
+            Log.d("Notification", "Notification onReceive DENY: ");
+        }
         launchIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
         Bundle data = intent.getExtras();
         data.putBoolean("tap", true);
